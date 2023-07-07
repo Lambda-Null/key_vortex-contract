feca7f17c86ecde628618541f4da4a0906939fb4
Lambda Null <lambda.null.42@gmail.com>
2023-07-07 18:41:05 -0400

    Base gem boilerplate

---
+++ spec/key_vortex/contract_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KeyVortex::Contract do
+  it "has a version number" do
+    expect(KeyVortex::Contract::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

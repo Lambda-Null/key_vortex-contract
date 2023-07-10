4c1b27e6230dc96574fbf46eec9bb1f0e013d1d1
Lambda Null <lambda.null.42@gmail.com>
2023-07-10 08:45:54 -0400

    Update contract to 0.2.0
    The interface has shifted wildly since the last version, this gets
    things more in line with where things are at now. I also matched the
    version to the main KeyVortex library, as that will make it easier to
    tell what this contract applies to.

--- lib/key_vortex/contract.rb
+++ lib/key_vortex/contract.rb
@@ -1,10 +1,31 @@
 # frozen_string_literal: true
 
-require_relative "contract/version"
+require "key_vortex/record"
+require "securerandom"
 
-module KeyVortex
-  module Contract
-    class Error < StandardError; end
-    # Your code goes here...
+class SampleRecord < KeyVortex::Record
+  field :sample, String, length: 10
+end
+
+RSpec.shared_context "an adapter" do
+  let(:store) do
+    KeyVortex.new(
+      subject,
+      SampleRecord
+    )
+  end
+
+  let(:record) do
+    SampleRecord.new(
+      key: SecureRandom.uuid,
+      sample: "foo"
+    )
+  end
+
+  it "stores and removes a key" do
+    store.save(record)
+    expect(store.find(record.key)).to eq(record)
+    subject.remove(record.key)
+    expect(store.find(record.key)).to be_nil
   end
 end
